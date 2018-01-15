47a3b6854253dcbf384356c5321b1ce66eea15cd
aeshes <mathesis@inbox.ru>
2018-01-15 20:49:44 +0300

    Final refactoring

--- Money2 TDD/Src/money.cpp
+++ Money2 TDD/Src/money.cpp
@@ -8,25 +8,25 @@ Money::Money(int amount_, string currency_)
 	this->currency_ = currency_;
 }
 
-shared_ptr<Money> Money::dollar(int amount)
+Money Money::dollar(int amount)
 {
-	return make_shared<Money>(amount, "USD");
+	return Money(amount, "USD");
 }
 
-shared_ptr<Money> Money::franc(int amount)
+Money Money::franc(int amount)
 {
-	return make_shared<Money>(amount, "CHF");
+	return Money(amount, "CHF");
 }
 
-shared_ptr<Money> Money::times(int value) const
+Money Money::times(int value) const
 {
-	return make_shared<Money>(amount * value, currency_);
+	return Money(amount * value, currency_);
 }
 
-bool Money::equals(shared_ptr<Money> money) const
+bool Money::equals(const Money& money) const
 {
-	return amount == money->amount
-		&& currency() == money->currency();
+	return amount == money.amount
+		&& currency() == money.currency();
 }
 
 string Money::currency() const
@@ -39,20 +39,20 @@ using ::testing::Eq;
 
 TEST(Money, TestDollarMultiplication)
 {
-	shared_ptr<Money> five = Money::dollar(5);
-	ASSERT_TRUE(five->times(2)->equals(Money::dollar(10)));
-	ASSERT_TRUE(five->times(3)->equals(Money::dollar(15)));
+	Money five = Money::dollar(5);
+	ASSERT_TRUE(five.times(2).equals(Money::dollar(10)));
+	ASSERT_TRUE(five.times(3).equals(Money::dollar(15)));
 }
 
 TEST(Money, TestFrancMultiplication)
 {
-	shared_ptr<Money> five = Money::franc(5);
-	ASSERT_TRUE(five->times(2)->equals(Money::franc(10)));
-	ASSERT_TRUE(five->times(3)->equals(Money::franc(15)));
+	Money five = Money::franc(5);
+	ASSERT_TRUE(five.times(2).equals(Money::franc(10)));
+	ASSERT_TRUE(five.times(3).equals(Money::franc(15)));
 }
 
 TEST(Money, TestCurrency)
 {
-	ASSERT_THAT(Money::dollar(1)->currency(), Eq("USD"));
-	ASSERT_THAT(Money::franc(1)->currency(), Eq("CHF"));
+	ASSERT_THAT(Money::dollar(1).currency(), Eq("USD"));
+	ASSERT_THAT(Money::franc(1).currency(), Eq("CHF"));
 }
--- Money2 TDD/Src/money.h
+++ Money2 TDD/Src/money.h
@@ -16,10 +16,10 @@ class Money
 {
 public:
 	Money(int amount_, string currency_);
-	static shared_ptr<Money> dollar(int amount);
-	static shared_ptr<Money> franc(int amount);
-	shared_ptr<Money> times(int value) const;
-	bool equals(shared_ptr<Money> money) const;
+	static Money dollar(int amount);
+	static Money franc(int amount);
+	Money times(int value) const;
+	bool equals(const Money& money) const;
 	string currency() const;
 protected:
 	int amount;
